3089c52a5e06e9e261201b7413eb4778ee7b4899
Ahmed Abdualhady <abdalhadyahmed2@gmail.com>
2026-09-19 17:53:21 +0200

    Add SSL for Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,10 @@ user: process.env.DB_USER,
 password: process.env.DB_PASSWORD,
 database: process.env.DB_NAME,
 port: process.env.DB_PORT,
+
+ssl: {
+rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
